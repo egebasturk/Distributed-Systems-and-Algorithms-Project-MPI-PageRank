419bf85734872bb24f847322b78d3a98f167d85d
egebasturk <alp.ege.basturk@gmail.com>
2019-05-19 12:09:52 +0300

    MPI libraries added

---
+++ Util.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <mpi.h>
+#include <math.h>
--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 #include "Util.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main (int argc, char *argv[])
+{
+    /// MPI initialization
+    MPI_Status s;
+    int size, rank, N;
+    MPI_Init (&argc, &argv);
+    MPI_Comm_size (MPI_COMM_WORLD, &size);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    int color;
+
+
+    // TODO: read these
+    int* vector = NULL;
+    int** matrix = NULL;
+    // TODO: Bcast according to read dimension
+    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);   // Bcast array dimension
+
+
+    // TODO: Cleanup
+    MPI_Finalize();
+}
+int* matrixVectorMultParallelMPI(int** mat, int* vector, int dim, int size, int myRank)
+{
+    int rowTileWidth = dim / size;
+    int myRowOffset = myRank * rowTileWidth;
+
+    int* recvBuffer = (int*)malloc(rowTileWidth * dim * sizeof(int));
+    MPI_Scatter(mat
+            , dim * rowTileWidth, MPI_INT, recvBuffer
+            , dim * rowTileWidth, MPI_INT, 0, MPI_COMM_WORLD);
+
 }
